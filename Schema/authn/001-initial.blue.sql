4ff5ea31941c2a9aa773314ede0dc6ac3ddf1a69
Kirit Sælensminde <k@kirit.com>
2016-07-13 12:39:23 +0700

    Add a login name which defaults to the user's identity.

--- Schema/authn/001-initial.blue.sql
+++ Schema/authn/001-initial.blue.sql
@@ -8,6 +8,9 @@ CREATE TABLE odin.credentials (
         ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE,
     CONSTRAINT credentials_pk PRIMARY KEY (identity_id),
 
+    -- Login name, the name used by the user in the log in form
+    login text NOT NULL,
+    CONSTRAINT credentials_login_ix UNIQUE (login),
     -- The password/hash value
     password text NOT NULL,
     -- The process for generating the password hash
@@ -17,9 +20,9 @@ CREATE TABLE odin.credentials (
 );
 
 
-CREATE TABLE odin.credentials_ledger (
+CREATE TABLE odin.credentials_password_ledger (
     identity_id text NOT NULL,
-    CONSTRAINT credentials_ledger_identity_fkey
+    CONSTRAINT credentials_password_ledger_identity_fkey
         FOREIGN KEY (identity_id)
         REFERENCES odin.identity (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE,
@@ -35,28 +38,28 @@ CREATE TABLE odin.credentials_ledger (
     hardens timestamp with time zone NULL,
     CONSTRAINT credentials_ledger_hardens_fkey
         FOREIGN KEY (identity_id, hardens)
-        REFERENCES odin.credentials_ledger (identity_id, changed) MATCH SIMPLE
+        REFERENCES odin.credentials_password_ledger (identity_id, changed) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE,
 
     annotation jsonb NOT NULL DEFAULT '{}'
 );
 CREATE FUNCTION odin.credentials_ledger_insert() RETURNS TRIGGER AS $body$
     BEGIN
-        INSERT INTO odin.credentials (identity_id, password, process, changed)
-                VALUES (NEW.identity_id, NEW.password, NEW.process, NEW.changed)
+        INSERT INTO odin.credentials (identity_id, login, password, process, changed)
+                VALUES (NEW.identity_id, NEW.identity_id, NEW.password, NEW.process, NEW.changed)
             ON CONFLICT (identity_id) DO UPDATE
                 SET password = EXCLUDED.password,
                     process = EXCLUDED.process,
                     changed = EXCLUDED.changed;
         IF NEW.hardens IS NOT NULL THEN
-            UPDATE odin.credentials_ledger SET password=NULL
+            UPDATE odin.credentials_password_ledger SET password=NULL
                 WHERE identity_id=NEW.identity_id AND changed=NEW.hardens;
         END IF;
         RETURN NULL;
     END;
     $body$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = odin;
 CREATE TRIGGER credentials_ledger_trigger
-    AFTER INSERT ON odin.credentials_ledger
+    AFTER INSERT ON odin.credentials_password_ledger
     FOR EACH ROW
     EXECUTE PROCEDURE odin.credentials_ledger_insert();
 
