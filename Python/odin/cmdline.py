74b3d5569ae8d1f6dd876f7463ed51c39e28985c
Kirit Sælensminde <k@kirit.com>
2016-07-14 12:05:25 +0700

    Print help text

--- Python/odin/cmdline.py
+++ Python/odin/cmdline.py
@@ -1,9 +1,27 @@
 
-SHORTOPTS = 'd:h:'
+SHORTOPTS = '?d:h:'
 OPTMAP = {
         '-d': 'dbname',
         '-h': 'host',
     }
+
+HELPTEXT = """Manage an Odin database
+
+    odin [opts] command [args]
+
+opts are one or more of:
+
+    -?                      Print this text
+    -h hostname             Postgres host
+    -d database             Database  name
+
+comand is one of:
+
+    user:
+            user username
+        Ensure the requested user is in the system
+
+"""
 
 
 def makedsn(opts, args):
