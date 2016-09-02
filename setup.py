00f42558417c59aeb9e044fe859183b3eaa910eb
Kirit Sælensminde <k@kirit.com>
2016-09-02 15:26:10 +0700

    Include the SQL files.

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 from distutils.core import setup
 
 setup(name='odin',
-        version='0.1.0',
+        version='0.1.1',
         description='Odin security system',
         author='Kirt Saelensminde',
         author_email='kirit@proteus-tech.com',
         url='https://www.kirit.com/Odin/',
-        packages=['odin'],
-        package_dir = {'': 'Python'},
         scripts=['Python/bin/odin'],
+        packages=['odin'],
+        package_dir={'': 'Python'},
+        data_files=[
+            ('share/odin/Schema', ['Schema/bootstrap.sql']),
+            ('share/odin/Schema/authn', ['Schema/authn/001-initial.blue.sql']),
+            ('share/odin/Schema/authz', ['Schema/authz/001-initial.blue.sql']),
+            ('share/odin/Schema/core', ['Schema/core/001-initial.blue.sql']),
+            ('share/odin/Schema/opts/full-name', ['Schema/opts/full-name/001-initial.blue.sql']),
+        ],
      )
