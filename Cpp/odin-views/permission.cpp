02e05837cd864907fea80fdd2bbd4111a962d96f
Kirit Sælensminde <k@kirit.com>
2016-09-14 12:40:25 +0700

    Add odin.view to allow checking of permissions through db connection.

---
+++ Cpp/odin-views/permission.cpp
@@ -0,0 +1,49 @@
+/*
+    Copyright 2016 Felspar Co Ltd. http://odin.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <odin/views.hpp>
+
+#include <fost/insert>
+#include <fostgres/sql.hpp>
+
+
+namespace {
+
+
+    const class permission : public fostlib::urlhandler::view {
+    public:
+        static const fostlib::jcursor userloc;
+
+        permission()
+        : view("odin.permission") {
+        }
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
+            const fostlib::json &config, const fostlib::string &path,
+            fostlib::http::server::request &req,
+            const fostlib::host &host
+        ) const {
+            fostlib::pg::connection cnx{fostgres::connection(config, req)};
+            fostlib::json where;
+            fostlib::insert(where, "identity_id", req[userloc].value());
+            fostlib::insert(where, "permission_slug", config["permission"]);
+            auto rs = cnx.select("odin.user_permission", where);
+            if ( rs.begin() == rs.end() ) {
+                return execute(config["forbidden"], path, req, host);
+            } else {
+                return execute(config["allowed"], path, req, host);
+            }
+        }
+    } c_permission;
+
+
+    const fostlib::jcursor permission::userloc("headers", "__user");
+
+
+}
+
