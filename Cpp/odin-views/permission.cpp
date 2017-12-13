e5144ea648b7aeae7290d68df9d867d89864bd1f
Kirit Sælensminde <k@kirit.com>
2017-12-13 13:58:58 +0700

    Add check that the view is secure before a permission check is done.

--- Cpp/odin-views/permission.cpp
+++ Cpp/odin-views/permission.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016 Felspar Co Ltd. http://odin.felspar.com/
+    Copyright 2016-2017 Felspar Co Ltd. http://odin.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -30,6 +30,12 @@ namespace {
         ) const {
             fostlib::pg::connection cnx{fostgres::connection(config, req)};
             fostlib::json where;
+            if ( not req[userloc] ) {
+                throw fostlib::exceptions::not_implemented(__func__,
+                    "The odin.permission view must be wrapped by an odin.secure "
+                    "view on the secure path so that there is a valid JWT to find "
+                    "the user ID in");
+            }
             fostlib::insert(where, "identity_id", req[userloc].value());
             fostlib::insert(where, "permission_slug", config["permission"]);
             auto rs = cnx.select("odin.user_permission", where);
