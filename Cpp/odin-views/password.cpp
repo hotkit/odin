fee72dd9968e43e7f13aead23703e7417b416cfe
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-07-10 13:47:03 +0700

    [WIP] reset password API

--- Cpp/odin-views/password.cpp
+++ Cpp/odin-views/password.cpp
@@ -26,30 +26,8 @@ namespace {
         if ( not message.empty() ) fostlib::insert(ret, "message", std::move(message));
         fostlib::mime::mime_headers headers;
         boost::shared_ptr<fostlib::mime> response(
-            new fostlib::text_body(fostlib::json::unparse(ret, true),
-                headers, "application/json"));
+            new fostlib::text_body(fostlib::json::unparse(ret, true), headers, "application/json"));
         return std::make_pair(response, code);
-    }
-
-    std::pair<boost::shared_ptr<fostlib::mime>, int> change_password(
-        fostlib::pg::connection &cnx,
-        fostlib::host remote_address,
-        f5::u8view reference,
-        f5::u8view username,
-        f5::u8view old_password,
-        f5::u8view new_password
-    ) {
-        if ( new_password.bytes() < 8u ) {
-            return respond("New password is too short");
-        }
-        auto user = odin::credentials(cnx, username, old_password, remote_address);
-        cnx.commit();
-        if ( user.isnull() ) {
-            return respond("Wrong password");
-        }
-        odin::set_password(cnx, reference, username, new_password);
-        cnx.commit();
-        return respond("", 200);
     }
 
 
@@ -64,29 +42,57 @@ namespace {
             fostlib::http::server::request &req,
             const fostlib::host &host
         ) const {
+            auto body_str = fostlib::coerce<fostlib::string>(
+                fostlib::coerce<fostlib::utf8_string>(req.data()->data()));
+            fostlib::json body = fostlib::json::parse(body_str);
+            fostlib::pg::connection cnx{fostgres::connection(config, req)};
+            const auto reference = req.headers()["__odin_reference"].value();
+            const auto remote_address = req.remote_address();
             if ( req.headers().exists("__user") ) {
                 const auto username = req.headers()["__user"].value();
-                auto body_str = fostlib::coerce<fostlib::string>(
-                    fostlib::coerce<fostlib::utf8_string>(req.data()->data()));
-                fostlib::json body = fostlib::json::parse(body_str);
                 if ( !body.has_key("new-password") || !body.has_key("old-password") ) {
                     return respond("Must supply both old and new password");
                 }
                 const auto old_password = fostlib::coerce<f5::u8view>(body["old-password"]);
                 const auto new_password = fostlib::coerce<f5::u8view>(body["new-password"]);
-                fostlib::pg::connection cnx{fostgres::connection(config, req)};
-                const auto reference = req.headers()["__odin_reference"].value();
-                const auto remote_address = req.remote_address();
-                return change_password(cnx, remote_address, reference, username, old_password, new_password);
-            } else {
-                return respond("No user is logged in");
+                auto user = odin::credentials(cnx, username, old_password, remote_address);
+                cnx.commit();
+                if ( user.isnull() ) return respond("Wrong password");
+                if ( new_password.bytes() < 8u ) return respond("New password is too short");
+                odin::set_password(cnx, reference, username, new_password);
+                cnx.commit();
+                return respond("", 200);
             }
+            return respond("No user is logged in");
         }
     } c_password_me;
 
 
+    const class reset_password : public fostlib::urlhandler::view {
+    public:
+        reset_password()
+        : view("odin.password.reset") {
+        }
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
+            const fostlib::json &config, const fostlib::string &path,
+            fostlib::http::server::request &req,
+            const fostlib::host &host
+        ) const {
+            auto body_str = fostlib::coerce<fostlib::string>(
+                fostlib::coerce<fostlib::utf8_string>(req.data()->data()));
+            fostlib::json body = fostlib::json::parse(body_str);
+            fostlib::pg::connection cnx{fostgres::connection(config, req)};
+            const auto reference = req.headers()["__odin_reference"].value();
+            const auto remote_address = req.remote_address();
+            // TODO:
+            // parse & validate JWT
+            // Fetch user
+            // if user exists then set new password
+            // logout user
+        }
+    } c_reset_password;
+
+
 }
-
-
-// const fostlib::urlhandler::view &odin::view::secure = c_secure;
 
