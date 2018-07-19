2551758e1a94b8806e445d2f4118367c12b50d53
Nut <nut@proteus-tech.com>
2018-07-19 19:14:42 +0700

    Set default value of google aud to null.

--- Cpp/odin/google.cpp
+++ Cpp/odin/google.cpp
@@ -26,9 +26,8 @@ fostlib::json odin::google::get_user_detail(f5::u8view user_token) {
     fostlib::json body = fostlib::json::parse(response->body()->data());
     auto aud = fostlib::coerce<fostlib::string>(body["aud"]);
     auto gg_aud = c_google_aud.value()["Client_ID"];
-    for ( std::size_t index{}; index < gg_aud.size(); ++index ) {
-        if ( aud == fostlib::coerce<fostlib::string>(gg_aud[index]) )
-            return body;
+    for ( const auto a : gg_aud ) {
+        if ( aud == fostlib::coerce<fostlib::string>(a) ) return body;
     }
     return fostlib::json();
 }
