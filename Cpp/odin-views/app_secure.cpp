8387554b54057f3d46ab192b09d7f57ae24d2c3b
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-09-12 15:52:47 +0700

    Not commit the transaction since it doesn't change anythings

--- Cpp/odin-views/app_secure.cpp
+++ Cpp/odin-views/app_secure.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016-2017 Felspar Co Ltd. http://odin.felspar.com/
+    Copyright 2018 Felspar Co Ltd. http://odin.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -38,7 +38,6 @@ namespace {
             fostlib::pg::connection cnx{fostgres::connection(config, req)};
             const auto app_id = paths[0];
             fostlib::json app = odin::app::get_detail(cnx, app_id);
-            cnx.commit();
             if ( app.isnull() )
                 throw fostlib::exceptions::not_implemented(__func__, "App not found");
             // Set the reference header
