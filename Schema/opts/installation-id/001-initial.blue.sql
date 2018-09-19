88232d1a3effce3b01bf34091a84d56f5b0ad42d
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-09-19 19:29:24 +0700

    Add more constraints, enable opts/installation-id module in test setup

--- Schema/opts/installation-id/001-initial.blue.sql
+++ Schema/opts/installation-id/001-initial.blue.sql
@@ -5,17 +5,25 @@ ALTER TABLE odin.identity  ADD COLUMN
     installation_id text NULL;
 
 CREATE TABLE odin.identity_installation_id_ledger (
-    reference text NOT NULL,
     identity_id text NOT NULL,
-    installation_id text NOT NULL
+    FOREIGN KEY (identity_id)
+    REFERENCES odin.identity (id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE,
+
+    installation_id text NOT NULL,
+    PRIMARY KEY (identity_id, installation_id),
+
+    changed timestamp with time zone NOT NULL DEFAULT now(),
+    pg_user text NOT NULL DEFAULT current_user,
+    reference text NOT NULL
 );
 
-CREATE FUNCTION odin.identity_installation_id_ledger_insert() RETRUN TRIGGER as $body$
+CREATE FUNCTION odin.identity_installation_id_ledger_insert() RETURNS TRIGGER as $body$
     BEGIN
         INSERT INTO odin.identity (id, installation_id)
             VALUES (NEW.identity_id, NEW.installation_id)
             ON CONFLICT (id) DO UPDATE SET
-                installation_id = EXCLUDED.installation_id
+                installation_id = EXCLUDED.installation_id;
         RETURN NULL;
     END;
     $body$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = odin;
@@ -23,4 +31,4 @@ CREATE FUNCTION odin.identity_installation_id_ledger_insert() RETRUN TRIGGER as 
 CREATE TRIGGER odin_installation_id_insert_trigger
     AFTER INSERT ON odin.identity_installation_id_ledger
     FOR EACH ROW
-    EXECUTE PROCEDURE odin.identity_installation_id_ledger_insert()
+    EXECUTE PROCEDURE odin.identity_installation_id_ledger_insert();
