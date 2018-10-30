e065db071d1172c90a48f8c54829adbdc4f83400
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-10-30 17:02:07 +0700

    Fix broken migration

--- Schema/opts/installation-id/002-fix-pk.blue.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO odin.migration VALUES('opts/installation-id', '002-fix-pk.blue.sql');
-
-ALTER TABLE odin.identity_installation_id_ledger
-    DROP CONSTRAINT identity_installation_id_ledger_pkey,
-    ADD PRIMARY KEY (reference);
