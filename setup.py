2679b5df1cc08fe092755a6496f90ac974f99bfa
nutVtobe <41340070+nutVtobe@users.noreply.github.com>
2018-12-17 19:53:10 +0700

    Fix typing

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='odin',
             ]),
             ('share/odin/Schema/app', [
                 'Schema/app/002-initial.blue.sql', 'Schema/app/003-app-role.blue.sql', 
-                'Schema/app/004-installation.blue.sql']),
+                'Schema/app/004-app-installation.blue.sql']),
             ('share/odin/Schema/opts/logout', [
                 'Schema/opts/logout/002-initial.blue.sql',
                 'Schema/opts/logout/003-fix-logout-count.blue.sql']),
