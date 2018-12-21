ccfdab6c9aaa9934e342e8e69bbf8b595eb316c1
Tle Ekkul <e.aryuth@gmail.com>
2018-12-21 14:50:57 +0700

    clang-format

--- Cpp/odin-views/password.cpp
+++ Cpp/odin-views/password.cpp
@@ -206,8 +206,10 @@ namespace {
             auto const body_str = fostlib::coerce<fostlib::string>(
                     fostlib::coerce<fostlib::utf8_string>(req.data()->data()));
             fostlib::json const body = fostlib::json::parse(body_str);
-            auto const hash_value = fostgres::datum(config["hash"], {}, body, req);
-            auto const verify = fostgres::datum(config["verify"], {}, body, req);
+            auto const hash_value =
+                    fostgres::datum(config["hash"], {}, body, req);
+            auto const verify =
+                    fostgres::datum(config["verify"], {}, body, req);
             if (not hash_value || not verify || hash_value != verify) {
                 return respond("Hashing failed", 422);
             }
