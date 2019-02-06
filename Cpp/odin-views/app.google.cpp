324f8049581a7dd5a4ae47325e70fd2a7569176a
Kirit Sælensminde <kirit@kirit.com>
2019-02-06 16:49:36 +0700

    Add files that are to be used for implemntation and tests for Google logins.

---
+++ Cpp/odin-views/app.google.cpp
@@ -0,0 +1,9 @@
+/**
+    Copyright 2019 Felspar Co Ltd. <http://odin.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#include <odin/app.hpp>
