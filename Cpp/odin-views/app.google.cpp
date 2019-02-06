644780be2491e7ed3fcbb8980ea3a1b208d28571
Kirit Sælensminde <kirit@kirit.com>
2019-02-06 17:08:05 +0700

    Implement the initial checks before the view can start.

--- Cpp/odin-views/app.google.cpp
+++ Cpp/odin-views/app.google.cpp
@@ -7,3 +7,47 @@
 
 
 #include <odin/app.hpp>
+#include <odin/views.hpp>
+
+#include <fost/log>
+
+
+namespace {
+
+
+    fostlib::module const c_odin_app_google{odin::c_odin_app, "google"};
+
+
+    const class g_login : public fostlib::urlhandler::view {
+      public:
+        g_login() : view("odin.app.google.login") {}
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
+                const fostlib::json &config,
+                const fostlib::string &path,
+                fostlib::http::server::request &req,
+                const fostlib::host &host) const {
+            auto logger{fostlib::log::debug(c_odin_app_google)};
+            if (req.method() != "POST") {
+                fostlib::json config;
+                fostlib::insert(config, "view", "fost.response.405");
+                fostlib::push_back(config, "configuration", "allow", "POST");
+                return execute(config, path, req, host);
+            }
+            if (not req.headers().exists("__app")
+                || not req.headers().exists("__user")) {
+                throw fostlib::exceptions::not_implemented(
+                        __PRETTY_FUNCTION__,
+                        "The odin.app.google.login view must be wrapped by an "
+                        "odin.app.secure view on the secure path so that there "
+                        "is a valid JWT to find the App ID in");
+            }
+            logger("__app", req.headers()["__app"]);
+            logger("__user", req.headers()["__user"]);
+            throw fostlib::exceptions::not_implemented(__PRETTY_FUNCTION__);
+        }
+
+    } c_g_login;
+
+
+}
