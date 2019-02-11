e32d81a3108bb00675b2f34a54ab55ddd4cf3a4b
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2019-02-11 15:32:35 +0700

    auto-generate data_files that will be included when pip install

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+datadir = os.path.join('Schema')
+datafiles = [(os.path.join('share', 'odin', d), [os.path.join(d,f) for f in files])
+    for d, folders, files in os.walk(datadir)]
+
 setup(name='odin',
         version='0.1.6.2',
         description='Odin security system',
@@ -11,26 +15,5 @@ setup(name='odin',
         packages=['odin'],
         package_dir={'': 'Python'},
         install_requires=['psycopg2-binary'],
-        data_files=[
-            ('share/odin/Schema', ['Schema/bootstrap.sql']),
-            ('share/odin/Schema/authn', ['Schema/authn/001-initial.blue.sql']),
-            ('share/odin/Schema/authz', [
-                'Schema/authz/001-initial.blue.sql',
-                'Schema/authz/002-view-user_permission.blue.sql']),
-            ('share/odin/Schema/core', ['Schema/core/000-initial.blue.sql']),
-            ('share/odin/Schema/opts/full-name', ['Schema/opts/full-name/001-initial.blue.sql']),
-            ('share/odin/Schema/opts/email', ['Schema/opts/email/001-initial.blue.sql']),
-            ('share/odin/Schema/opts/forgotten-password', ['Schema/opts/forgotten-password/001-initial.blue.sql']),
-            ('share/odin/Schema/opts/facebook', ['Schema/opts/facebook/001-initial.blue.sql']),
-            ('share/odin/Schema/opts/google', ['Schema/opts/google/001-initial.blue.sql']),
-            ('share/odin/Schema/opts/installation-id', [
-                'Schema/opts/installation-id/001-initial.blue.sql'
-            ]),
-            ('share/odin/Schema/app', [
-                'Schema/app/002-initial.blue.sql', 'Schema/app/003-app-role.blue.sql', 
-                'Schema/app/004-app-installation.blue.sql']),
-            ('share/odin/Schema/opts/logout', [
-                'Schema/opts/logout/002-initial.blue.sql',
-                'Schema/opts/logout/003-fix-logout-count.blue.sql']),
-        ],
+        data_files=datafiles,
      )
