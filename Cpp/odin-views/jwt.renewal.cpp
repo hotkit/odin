7b2c1424b3a6eae3e5e07dcc1e799ea202a8d850
Semooze <cloudtimewater@gmail.com>
2019-06-25 00:12:44 +0700

    Refactor

--- Cpp/odin-views/jwt.renewal.cpp
+++ Cpp/odin-views/jwt.renewal.cpp
@@ -45,22 +45,16 @@ namespace {
             auto const jwt_user = req.headers()["__user"].value();
             
             // Check whether it is APP JWT or non APP JWT
+
+            fostlib::timestamp exp;
+            fostlib::utf8_string token;
             if (req.headers().exists("__app")) {
                 
                 auto jwt = odin::app::mint_user_jwt(
                         jwt_user, app_id,
                         fostlib::coerce<fostlib::timediff>(config["expires"]));
-                fostlib::mime::mime_headers headers;
-                headers.add(
-                        "Expires",
-                        fostlib::coerce<fostlib::rfc1123_timestamp>(jwt.second)
-                                .underlying()
-                                .underlying());
-
-                boost::shared_ptr<fostlib::mime> response(new fostlib::text_body(
-                        jwt.first, headers, L"application/jwt"));
-                return std::make_pair(response, 200);
-
+                token = jwt.first;
+                exp = jwt.second;
             } else {
                 
                 fostlib::pg::connection cnx{fostgres::connection(config, req)};
@@ -72,44 +66,41 @@ namespace {
                 auto row = rs.begin();
                 auto record = *row;
 
-        
                 fostlib::json user;
                 for (std::size_t index{0}; index < record.size(); ++index) {
                     const auto parts = fostlib::split(data.first[index], "__");
-                    if (parts.size() && parts[parts.size() - 1] == "tableoid") continue;
                     fostlib::jcursor pos;
                     for (const auto &p : parts) pos /= p;
                     fostlib::insert(user, pos, record[index]);
                     
                 }
 
-                std::map<fostlib::string, fostlib::json> test;
-                test.insert(std::pair<fostlib::string, fostlib::json>("identity", user));
-                fostlib::json right_constructed_user = test;
+                std::map<fostlib::string, fostlib::json> format;
+                format.insert(std::pair<fostlib::string, fostlib::json>("identity", user));
+                fostlib::json identity_user = format;
 
-                auto jwt_response(odin::mint_login_jwt(right_constructed_user));
-                auto exp = jwt_response.expires(
+                auto jwt_response(odin::mint_login_jwt(identity_user));
+                exp = jwt_response.expires(
                     fostlib::coerce<fostlib::timediff>(config["expires"]),
                     false);
+                token = jwt_response.token(odin::c_jwt_secret.value().data());
 
-                fostlib::mime::mime_headers headers;
-                headers.add(
-                    "Expires",
-                    fostlib::coerce<fostlib::rfc1123_timestamp>(exp)
-                            .underlying()
-                            .underlying());
-
-                boost::shared_ptr<fostlib::mime> response(new fostlib::text_body(
-                    fostlib::utf8_string(jwt_response.token(
-                            odin::c_jwt_secret.value().data())),
-                    headers, L"application/jwt"));
-                
-                return std::make_pair(response, 200);
             }
+
+            fostlib::mime::mime_headers headers;
+            headers.add(
+                        "Expires",
+                        fostlib::coerce<fostlib::rfc1123_timestamp>(exp)
+                                .underlying()
+                                .underlying());
+
+            boost::shared_ptr<fostlib::mime> response(new fostlib::text_body(
+                        token, headers, L"application/jwt"));
+
+            return std::make_pair(response, 200);
         }
 
     } c_jwt_renewal;
 }
-
 
 const fostlib::urlhandler::view &odin::view::jwt_renewal = c_jwt_renewal;
